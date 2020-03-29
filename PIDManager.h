330019825b502de77e7065a1860623664400cbda
suneel kumar <45797776+suneel3@users.noreply.github.com>
2020-03-29 17:16:53 +0530

    Add files via upload

---
+++ PIDManager.h
@@ -0,0 +1,62 @@
+
+
+#ifndef _PIDManager_h
+#define _PIDManager_h
+
+#include<stdio.h>
+#include<unistd.h>
+#define MIN_PID 100
+#define MAX_PID 1000
+#define ARR_SIZE MAX_PID-MIN_PID+1
+
+int pid_status[ARR_SIZE];       //data structure to store or maintain the pid statuses.
+
+int allocate_map()        //initializing pid statuses
+{
+  int i;
+  for(i=0;i<ARR_SIZE;i++)
+  {
+    pid_status[i]=0;
+  }
+  return 0;               //return 0 indicating statuses have initialized.
+}
+
+int allocate_pid()
+{
+  int i;
+  for(i=0;i<ARR_SIZE;i++)
+  {
+    if(pid_status[i]==0)   //checking if pid is free
+    {
+      pid_status[i]=1;     //changing status of pid
+      return i+100;        //returning pid
+    }
+  }
+  return 1;                //if pids are not free, 1 is returned.
+}
+
+void release_pid(int p)
+{
+  int pid=p-100;            //taking pid from parameter.
+  pid_status[pid]=0;        //changing pid status.
+}
+
+void display()
+{
+  int i,c=0;
+  printf("\n----------------------------------------");
+  for(i=0;i<ARR_SIZE;i++)
+  {
+    if(pid_status[i]==1)              //checking for pids which are in use.
+    {
+      printf("\n[%d] = %d\t",i+100,pid_status[i]);
+      c++;
+    }
+  }
+  printf("\nPids in Use: %d",c);
+  printf("\n----------------------------------------\n\n");
+
+}
+#endif
+  
+
---
+++ PROJECT.C
@@ -0,0 +1,95 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<pthread.h>
+#include "PIDManager.h"
+
+struct proc       //structure for thread which represents process's data
+{
+  int sno;        //serial number
+  int pid;        //pid value
+  int sl_time;    //sleep time
+};
+
+int pid_check(int pid)
+{
+  if(pid>=100)                        //checking whether pid is assigned or not.
+  {
+    printf("\nPid %d assigned successfully...\n",pid);
+    return 0;                               //returning 0 to indicate pid is set.
+  }
+  else if(pid==1)                     //if returned is 1, pid is not set (indicating pids are not free).
+  {
+    printf("\npid allocation failed...\n"); 
+    return 1;                         //returning 1 to say the same.
+  }
+  else                              //if something else happened.
+  {
+    printf("\nSomething went wrong... pid: %d is returned...",pid);
+    return 1;
+  }
+}
+
+int set_sleep_time()
+{
+  return rand()%10;                      //giving sleep time in range 0-10 secs.
+}
+
+void *thread_fun(void *pro)               //In each thread, this body of code executes.
+{
+  struct proc *p = (struct proc *) pro;
+  printf("\n%d: Fetching pid...\n",p->sno);
+  p->pid = allocate_pid();                 //fetching pid
+  
+  while(pid_check(p->pid)==1)             //until pid is assigned, this loop will run.
+  {
+    sleep(5);                             //sleeps for 5 secs as pid is not assigned.
+    printf("\n%d: Fetching pid...\n",p->sno);   
+    p->pid = allocate_pid();              //trying to fetch pid once more.
+  }
+  display();                              //display the pids statuses.
+  
+  p->sl_time = set_sleep_time();          //setting of sleep time for every process or thread.
+  printf("\n%d: pid = %d,sl_time = %d\n",p->sno,p->pid,p->sl_time);
+  printf("\n%d - %d: Entered Sleep time...\n",p->sno,p->pid);
+  sleep(p->sl_time);                                                    //every thread sleep for its allocated time.
+  printf("\n%d - %d: Exited Sleep time...\n",p->sno,p->pid);
+  release_pid(p->pid);                                    //after sleep time completes, every thread releases its pid.
+  display();
+}
+
+int main()
+{
+  int i, no_of_threads;
+  printf("Enter no of threads: ");
+  scanf("%d",&no_of_threads);               //getting no of threads from user input.
+  printf("...program getting stared...\n");
+  
+  int x = allocate_map();                   //initializing pid statuses.
+  if(x==0)                                  //checking whether they get initialized or not.
+    printf("Map allocated...\n");
+  else
+    printf("Problem in initializing statuses...\n");
+  
+  struct proc pr[no_of_threads];               //creating structures for threads to store data of a thread of the number given by user.
+  pthread_t pr_threads[no_of_threads];              //creating threads references of number given by user.
+  
+  for(i=0;i<no_of_threads;i++)
+  {
+    pr[i].sno = i;                                  //giving every structure its serial number, starting from 0.
+    pthread_create(&pr_threads[i],NULL,thread_fun,&pr[i]);           //creating threads with function associated with every thread.
+  }
+  
+  for(i=0;i<no_of_threads;i++)              
+  {
+    pthread_join(pr_threads[i],NULL);                     //waiting for every thread to get execute.
+  }
+  
+  printf("\n...Program Successfully terminated...\n");
+}
+  
+  
+  
+  
+    
+    
+    
